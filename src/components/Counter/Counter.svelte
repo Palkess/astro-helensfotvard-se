b7e8959fc5893ac38a3f2b5300d25c1e742c2281
Jonas Andersson <jonas.andersson.web@gmail.com>
2026-03-07 14:33:19 +0100

    Init commit

---
+++ src/components/Counter/Counter.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let count = $state(0);
+</script>
+
+<div class="counter">
+    <h2>Counter: {count}</h2>
+    <button class="rounded-2xl border bg-green-700" onclick={() => count++}>Increment</button>
+    <button class="rounded-2xl border bg-red-700" onclick={() => count--}>Decrement</button>
+</div>
