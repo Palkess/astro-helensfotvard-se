a2df94ac39cdaa3b3c09216f3d4afd438058f6f3
Palkess <jonas.andersson.web@gmail.com>
2026-03-15 18:58:34 +0100

    Publishing the website to beta.helensfotvard.se

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://beta.helensfotvard.se',
     integrations: [svelte()],
 
     vite: {
